09493df3e52862368cb407147832380aa885b04a
Joaquin <jeva1991@gmail.com>
2021-10-11 19:44:56 -0300

    Adding edge case

--- src/test/java/leetcode/array/SpiralOrderTest.java
+++ src/test/java/leetcode/array/SpiralOrderTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -19,6 +20,15 @@ class SpiralOrderTest {
                 ),
                 Arguments.of(
                         new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}}, Arrays.asList(1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7)
+                ),
+                Arguments.of(
+                        new int[][]{{1}, {2}, {3}, {4}}, Arrays.asList(1, 2, 3, 4)
+                ),
+                Arguments.of(
+                        new int[][]{{1, 2, 3, 4}}, Arrays.asList(1, 2, 3, 4)
+                ),
+                Arguments.of(
+                        new int[][]{{}}, new ArrayList<>()
                 )
         );
     }
